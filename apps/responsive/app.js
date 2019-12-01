ce99c2465d61904071ac6d5d70cb66d5ea0a08d6
Jelizaveta Potapova <jelizaveta.potapova@devbridge.com>
2019-12-01 17:19:27 +0200

    Refactor responsive example

--- apps/responsive/app.js
+++ apps/responsive/app.js
@@ -1,27 +1,35 @@
 const button = document.querySelector("button");
+
 const narrow = () => {
   alert("Narrow screen js running");
 };
+
 const wide = () => {
   alert("Wide screen js running");
 };
 
-var mq = window.matchMedia("(min-width: 600px)");
-
-if (mq.matches) {
-  button.addEventListener("click", wide);
-  button.removeEventListener("click", narrow);
-} else {
+const handleNarrow = () => {
   button.addEventListener("click", narrow);
   button.removeEventListener("click", wide);
+};
+
+const handleWide = () => {
+  button.addEventListener("click", wide);
+  button.removeEventListener("click", narrow);
+};
+
+const mq = window.matchMedia("(min-width: 600px)");
+
+if (mq.matches) {
+  handleWide();
+} else {
+  handleNarrow();
 }
 
 mq.addListener(function(e) {
   if (e.matches) {
-    button.addEventListener("click", wide);
-    button.removeEventListener("click", narrow);
+    handleWide();
   } else {
-    button.addEventListener("click", narrow);
-    button.removeEventListener("click", wide);
+    handleNarrow();
   }
 });
