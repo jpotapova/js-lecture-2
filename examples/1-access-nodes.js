be0f4ceec77b436662b6c66aeb678e5218530cac
Jelizaveta Potapova <jelizaveta.potapova@devbridge.com>
2019-12-02 12:19:05 +0200

    Update examples

---
+++ examples/1-access-nodes.js
@@ -0,0 +1,13 @@
+// NAVIGATE DOM -------------------------
+someNode.children; // select children
+someNode.firstElementChild; // select first child
+someNode.nextElementSibling; // select next sibling
+someNode.parentElement; // select parent
+
+// SEARCH DOM ------------------------------
+// select 1 node by its id
+document.getElementById("my-id-");
+// select first node that matches selector
+document.querySelector("my-selector");
+// select all nodes that match selector
+document.querySelectorAll("my-selector");
